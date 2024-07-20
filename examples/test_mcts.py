094a2b3a359624b347ba2690b7afa204c4282a32
Yunfan Lu <yunfan.lu@dyson.com>
2024-07-20 22:35:27 +0800

    Improve comments

--- examples/test_mcts.py
+++ examples/test_mcts.py
@@ -1,25 +1,21 @@
 import numpy as np
 
-# from robotics_algorithm.env.grid_world import GridWorld
+from robotics_algorithm.env.frozen_lake import FrozenLake
 from robotics_algorithm.env.cliff_walking import CliffWalking
 from robotics_algorithm.planning import MCTS
 
-
-env = CliffWalking(dense_reward=True)  # For online tree search, dense reward needs to be enabled.
-state = env.reset()
+env = FrozenLake(dense_reward=True)  # For online tree search, dense reward needs to be enabled.
+# env = CliffWalking(dense_reward=True)  # For online tree search, dense reward needs to be enabled.
+state = env.reset(random_env=False)
 env.render()
 
 planner = MCTS(env)
 
-# Plan
-# policy = planner.run(env, max_depth=5)
-
 # Execute
 path = []
 while True:
-    # choose action according to epsilon-greedy policy
+    # Call tree search online
     action = planner.run(state)
-    # action = np.random.choice(env.action_space, p=action_probs)  # choose action
     new_state, reward, term, trunc, info = env.step(action)
 
     print(state)
